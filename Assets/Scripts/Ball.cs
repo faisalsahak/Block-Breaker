8e709d54caffed173b1a14e18bc9fb847c3d2293
user <user@users-MacBook-Pro.local>
2019-05-26 15:06:25 -0700

    added audio to the game

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -43,4 +43,13 @@ public class Ball : MonoBehaviour
     	transform.position = paddlePos + paddleToBallVector;
         
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(hasStarted)
+            GetComponent<AudioSource>().Play();
+    }
+
+
+
 }
