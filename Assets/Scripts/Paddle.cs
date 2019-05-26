a09148cf9ce90ce1a0d2f6954342d5aea32f89aa
user <user@users-MacBook-Pro.local>
2019-05-26 11:13:30 -0700

    added a script to the paddle whichis now able to move freely on the x-axis at the position of the mouse and able to collide with the ball

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	[SerializeField] float screenWithInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+    	Vector2 paddlePos = new Vector2(Input.mousePosition.x/Screen.width*screenWithInUnits,0.25f);
+    	transform.position = paddlePos;
+        
+    }
+}
