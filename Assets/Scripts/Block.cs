df59a7d65dfc125d8d928a8731a1bfa60be02846
user <user@users-MacBook-Pro.local>
2019-05-27 23:03:24 -0700

    added extra block sprites for damage, so that each time a block is hit a difference block is show based on the damage

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,17 +5,48 @@ using UnityEngine;
 public class Block : MonoBehaviour{
 
 	[SerializeField] AudioClip breakSound;
+	[SerializeField] int maxHits;
+	[SerializeField] Sprite[] hitSprites;
+
+
+	[SerializeField] int timesHit; // serialized for debug purposes
 
 	//cached reference
 	Level level;
 
 	private void Start(){
+		CountBreakableBlocks();
+	}
+
+	private void CountBreakableBlocks(){
+
 		level = FindObjectOfType<Level>();
-		level.CountBreakableBlocks();
+		if(tag == "Breakable"){
+			level.CountBlocks();
+		}
 	}
     
     private void OnCollisionEnter2D(Collision2D collision){
+    	if(tag == "Breakable"){
+    		HandleHit();
+    	}
+    }
 
+    private void HandleHit(){
+    	timesHit++;
+		if(timesHit >= maxHits){
+			DestroyBlock();
+		}else{
+			ShowNextHitSprite();
+		}
+    }
+
+    private void ShowNextHitSprite(){
+    	int spriteIndex = timesHit-1;
+    	GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    private void DestroyBlock(){
     	FindObjectOfType<GameStatus>().AddToScore();
 
     	AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
